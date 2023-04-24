0cde4c64f9a878fa32f5b360c23000e82f38e3d4
Raagul S <58085092+IM-TECHNO@users.noreply.github.com>
2023-04-24 12:22:44 +0530

    Upload Install Script

---
+++ install-carla.sh
@@ -0,0 +1,17 @@
+echo "Installing Carla Simulator"
+echo "\n"
+if nc -zw1 keyserver.ubuntu.com 80; then
+  echo "Connection successful"
+  echo "\n"
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1AF1527DE64CB8D9
+  sudo add-apt-repository "deb [arch=amd64] http://dist.carla.org/carla $(lsb_release -sc) main"
+  sudo apt-get update 
+  sudo apt-get install carla-simulator
+  cd /opt/carla-simulator
+
+else
+  echo "Connection unsuccessful"
+  echo "\n"
+  echo "Firewall detected!!! Use your OWN hotspot"
+  exit 0
+fi
